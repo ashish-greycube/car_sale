f21015083737a33038cedbe7bcf37d1a73a2747b
ashish-greycube <mr.ashish.shah@gmail.com>
2019-01-03 11:42:48 +0530

    init changes

--- car_sale/__init__.py
+++ car_sale/__init__.py
@@ -7,10 +7,11 @@ __version__ = '0.0.1'
 def carsale_has_customer(self):
     if self:
         if self.name:
-            if self.linked_quotation or self.linked_sales_order:
-                return None
-            else:
-                return frappe.db.get_value("Customer", {"lead_name": self.name})
+            if self.inquiry_item:
+                if self.linked_quotation or self.linked_sales_order:
+                    return None
+                else:
+                    return frappe.db.get_value("Customer", {"lead_name": self.name})
 
 from erpnext.crm.doctype.lead.lead import Lead
 Lead.has_customer = carsale_has_customer
