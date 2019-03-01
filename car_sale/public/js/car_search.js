c68de1009608e02a6eace01157be2c5ffa9fb39d
ashish-greycube <mr.ashish.shah@gmail.com>
2019-03-01 14:08:07 +0530

    serial no search

--- car_sale/public/js/car_search.js
+++ car_sale/public/js/car_search.js
@@ -1,77 +1,81 @@
 frappe.ui.form.on(cur_frm.doctype, {
-    search_group: function(frm){
+    search_group: function (frm) {
         if (cur_frm.doc.search_group) {
             frappe.call({
                 method: "car_sale.api.get_template_name",
-                args: { search_group: cur_frm.doc.search_group },
+                args: {
+                    search_group: cur_frm.doc.search_group
+                },
                 callback: function (r) {
                     if (r.message) {
                         cur_frm.fields_dict.search_template.df.options = r.message;
                         cur_frm.set_value('search_template', null);
                         cur_frm.refresh_field("search_template");
-    
-                        
-                    }else{
+
+
+                    } else {
                         cur_frm.fields_dict.search_template.df.options = '';
                         cur_frm.set_value('search_template', null);
                         cur_frm.refresh_field("search_template");
-    
+
                     }
-    
-                    cur_frm.fields_dict.search_category.df.options =''
+
+                    cur_frm.fields_dict.search_category.df.options = ''
                     cur_frm.set_value('search_category', null);
                     cur_frm.refresh_field("search_category");
-    
-                    cur_frm.fields_dict.search_model.df.options =''
+
+                    cur_frm.fields_dict.search_model.df.options = ''
                     cur_frm.set_value('search_model', null);
                     cur_frm.refresh_field("search_model");
-    
-                    cur_frm.fields_dict.search_color.df.options=''
+
+                    cur_frm.fields_dict.search_color.df.options = ''
                     cur_frm.set_value('search_color', null);
                     cur_frm.refresh_field("search_color");
-    
+
                 }
             })
-            
-        }
-        else{
+
+        } else {
             cur_frm.fields_dict.search_template.df.options = '';
             cur_frm.set_value('search_template', null);
             cur_frm.refresh_field("search_template");
 
         }
     },
-	search_template: function(frm){
+    search_template: function (frm) {
         frappe.call({
             method: "car_sale.api.get_category_name",
-            args: { search_template: cur_frm.doc.search_template },
+            args: {
+                search_template: cur_frm.doc.search_template
+            },
             callback: function (r) {
                 if (r.message) {
                     cur_frm.fields_dict.search_category.df.options = r.message;
                     cur_frm.set_value('search_category', null);
                     cur_frm.refresh_field("search_category");
 
-                    cur_frm.fields_dict.search_model.df.options =''
+                    cur_frm.fields_dict.search_model.df.options = ''
                     cur_frm.set_value('search_model', null);
                     cur_frm.refresh_field("search_model");
 
-                    cur_frm.fields_dict.search_color.df.options=''
+                    cur_frm.fields_dict.search_color.df.options = ''
                     cur_frm.set_value('search_color', null);
                     cur_frm.refresh_field("search_color");
-                    
+
                 }
 
             }
         })
     },
-    search_category: function(frm){
+    search_category: function (frm) {
         frappe.call({
             method: "car_sale.api.get_model_name",
-            args: { search_template: cur_frm.doc.search_template,
-                search_category:cur_frm.doc.search_category
-             },
+            args: {
+                search_template: cur_frm.doc.search_template,
+                search_category: cur_frm.doc.search_category
+            },
             callback: function (r) {
-                if (r.message){
+                if (r.message) {
                     cur_frm.fields_dict.search_model.df.options = r.message;
                     cur_frm.set_value('search_model', null);
                     cur_frm.refresh_field("search_model");
@@ -80,62 +84,55 @@ frappe.ui.form.on(cur_frm.doctype, {
             }
         })
     },
-    search_model: function(frm){
+    search_model: function (frm) {
         frappe.call({
             method: "car_sale.api.get_color_name",
-            args: { search_template: cur_frm.doc.search_template,
-                search_category:cur_frm.doc.search_category,
-                search_model:cur_frm.doc.search_model
-             },
+            args: {
+                search_template: cur_frm.doc.search_template,
+                search_category: cur_frm.doc.search_category,
+                search_model: cur_frm.doc.search_model
+            },
             callback: function (r) {
                 if (r.message) {
                     cur_frm.fields_dict.search_color.df.options = r.message;
-                    cur_frm.refresh_field("search_color");                  
+                    cur_frm.refresh_field("search_color");
                 }
 
             }
         })
     },
-    add: function(frm){
-        if (cur_frm.doc.search_group == undefined || cur_frm.doc.search_group == '') 
-        {
+    add: function (frm) {
+        if (cur_frm.doc.search_group == undefined || cur_frm.doc.search_group == '') {
             frappe.msgprint(__("Field Group cannot be empty"));
             return;
-        }
-        else if (cur_frm.doc.search_template == undefined || cur_frm.doc.search_template == '') 
-        {
+        } else if (cur_frm.doc.search_template == undefined || cur_frm.doc.search_template == '') {
             frappe.msgprint(__("Field Brand cannot be empty"));
             return;
-        }else if (cur_frm.doc.search_category == undefined || cur_frm.doc.search_category == '') 
-        {
+        } else if (cur_frm.doc.search_category == undefined || cur_frm.doc.search_category == '') {
             frappe.msgprint(__("Field Category cannot be empty"));
             return;
-        }
-        else if(cur_frm.doc.search_model == undefined || cur_frm.doc.search_model == '')
-        {
+        } else if (cur_frm.doc.search_model == undefined || cur_frm.doc.search_model == '') {
             frappe.msgprint(__("Field Model cannot be empty"));
             return;
-        } 
-        else if(cur_frm.doc.search_color == undefined || cur_frm.doc.search_color == '')
-        {
+        } else if (cur_frm.doc.search_color == undefined || cur_frm.doc.search_color == '') {
             frappe.msgprint(__("Field Color cannot be empty"));
             return;
-        } 
+        }
 
         frappe.call({
             method: "car_sale.api.get_search_item_name",
-            args: { 
+            args: {
                 search_template: cur_frm.doc.search_template,
-                search_category:cur_frm.doc.search_category,
-                search_model:cur_frm.doc.search_model,
-                search_color:cur_frm.doc.search_color,
-             },
+                search_category: cur_frm.doc.search_category,
+                search_model: cur_frm.doc.search_model,
+                search_color: cur_frm.doc.search_color,
+            },
             callback: function (r) {
                 if (r.message) {
                     if (r.message[0]) {
 
                         if (cur_frm.doc.items[0]) {
-                            if (cur_frm.doc.items[0].item_code==undefined) {
+                            if (cur_frm.doc.items[0].item_code == undefined) {
                                 cur_frm.doc.items.splice(cur_frm.doc.items[0], 1)
                             }
                         }
@@ -154,11 +151,11 @@ frappe.ui.form.on(cur_frm.doctype, {
                         cur_frm.set_value('search_category', null);
                         cur_frm.refresh_field("search_category");
 
-                        cur_frm.fields_dict.search_model.df.options =''
+                        cur_frm.fields_dict.search_model.df.options = ''
                         cur_frm.set_value('search_model', null);
                         cur_frm.refresh_field("search_model");
 
-                        cur_frm.fields_dict.search_color.df.options=''
+                        cur_frm.fields_dict.search_color.df.options = ''
                         cur_frm.set_value('search_color', null);
                         cur_frm.refresh_field("search_color")
                     }
@@ -166,32 +163,26 @@ frappe.ui.form.on(cur_frm.doctype, {
             }
         })
     },
-    add_serial_no_item: function(frm){
-        console.log("as per data from serial_no doctype",cur_frm.doc.search_serial_no,cur_frm.doc.serial_no_warehouse)
-        if (cur_frm.doc.search_serial_no && (cur_frm.doc.serial_no_item!=undefined || cur_frm.doc.serial_no_item!='') ) {
-            if ((cur_frm.doc.items[0]) && (cur_frm.doc.items[0].item_code==undefined || cur_frm.doc.items[0].item_code=='' || cur_frm.doc.items[0].item_code==null)) {
+    add_serial_no_item: function (frm) {
+        if (cur_frm.doc.search_serial_no && (cur_frm.doc.serial_no_item != undefined || cur_frm.doc.serial_no_item != '')) {
+            if ((cur_frm.doc.items[0]) && (cur_frm.doc.items[0].item_code == undefined || cur_frm.doc.items[0].item_code == '' || cur_frm.doc.items[0].item_code == null)) {
                 cur_frm.doc.items.splice(cur_frm.doc.items[0], 1)
             }
-                var child = cur_frm.add_child("items");
-                frappe.model.set_value(child.doctype, child.name, "item_code", cur_frm.doc.serial_no_item)
-                frappe.model.set_value(child.doctype, child.name, "serial_no", cur_frm.doc.search_serial_no)
-                if (cur_frm.doc.serial_no_warehouse!=null && cur_frm.doc.serial_no_warehouse!=undefined && cur_frm.doc.serial_no_warehouse!='') {
-                    if ( child.warehouse!=cur_frm.doc.serial_no_warehouse) {
-                    // frappe.model.set_value(child.doctype, child.name, "warehouse", cur_frm.doc.serial_no_warehouse)
-                       }
-                 }else{
-                    // frappe.model.set_value(child.doctype, child.name, "warehouse", undefined)
-                 }
-                 cur_frm.refresh_field("items")
+            var child = cur_frm.add_child("items");
+            frappe.model.set_value(child.doctype, child.name, "item_code", cur_frm.doc.serial_no_item)
+            frappe.model.set_value(child.doctype, child.name, "serial_no", cur_frm.doc.search_serial_no)
+            $.extend(child, {
+                "warehouse": cur_frm.doc.serial_no_warehouse
+            });
+            cur_frm.refresh_field("items")
 
-                 cur_frm.set_value('search_serial_no', undefined);
+            cur_frm.set_value('search_serial_no', undefined);
             cur_frm.set_value('serial_no_item', undefined);
             cur_frm.set_value('serial_no_warehouse', undefined);
             cur_frm.refresh_field("search_serial_no")
-            cur_frm.refresh_field("serial_no_item") 
-            cur_frm.refresh_field("serial_no_warehouse") 
-        }
-        else{
+            cur_frm.refresh_field("serial_no_item")
+            cur_frm.refresh_field("serial_no_warehouse")
+        } else {
             frappe.msgprint(__("Select appropriate serial no."));
             return;
         }
