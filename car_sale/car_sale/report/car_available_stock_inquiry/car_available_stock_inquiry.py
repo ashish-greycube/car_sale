9314743e76412f0e85241d65f96b554895df297b
ashish-greycube <mr.ashish.shah@gmail.com>
2019-05-03 20:30:25 +0530

    change in query and currency to float

--- car_sale/car_sale/report/car_available_stock_inquiry/car_available_stock_inquiry.py
+++ car_sale/car_sale/report/car_available_stock_inquiry/car_available_stock_inquiry.py
@@ -14,12 +14,18 @@ def execute(filters=None):
 	return columns, data
 
 def get_column():
-	return [
-		_("Car Name") + ":Link/Item:200",
-		_("Warehouse") + ":Link/Warehouse:100",
-		_("Qty") + ":Int:40",
-		_("Selling Rate") + ":Currency:100"
-	]
+	# return [
+	# 	_("Car Name") + ":Link/Item:200",
+	# 	_("Warehouse") + ":Link/Warehouse:100",
+	# 	_("Qty") + ":Int:40",
+	# 	_("Selling Rate") + ":Float:60"
+	# ]
+    return [
+        {"label": _("Car Name"), 'width': 200, "fieldname": "CarName", 'fieldtype': 'Link/Item'},
+		{"label":_("Warehouse"), 'width': 100, "fieldname": "Warehouse", 'fieldtype': 'Link/Warehouse'},
+		{"label": _("Qty"), 'width': 40, "fieldname": "Quantity", 'fieldtype': 'Int'},
+        {"label": _("Selling Rate"), "fieldname": "SellingRate", 'fieldtype': 'Float', 'width': 100, 'precision': 1}
+    ]
 
 def get_car_available_stock_inquiry(filters):
 	print '-------------before'
@@ -61,75 +67,87 @@ T.CarName,
 
 T.Warehouse,
 
-T.Quantity,
+count(T.serial_no) as Quantity,
 
 T.SellingRate
 
-FROM ( SELECT
+FROM
 
-TS.item_name as CarName,
+( SELECT
 
-TS.warehouse as Warehouse,
+       TS.item_name as CarName,
 
-count(serial_no) as Quantity,
+       TS.warehouse as Warehouse,
 
-TIP.price_list_rate AS SellingRate,
+       TS.serial_no,
 
-TI.variant_of,
+       IFNULL(TIP.price_list_rate,0.00) AS SellingRate,
 
-TI.item_group,
+       TI.variant_of,
 
-MAX(IF (TVA.attribute ='Color',TVA.attribute_value,'')) AS Color,
+       TI.item_group,
 
-MAX(IF (TVA.attribute ='model',TVA.attribute_value,'')) AS Model,
+       MAX(IF (TVA.attribute ='Color',TVA.attribute_value,'')) AS Color,
 
-MAX(IF (TVA.attribute ='Category',TVA.attribute_value,'')) AS Category
+       MAX(IF (TVA.attribute ='model',TVA.attribute_value,'')) AS Model,
+
+       MAX(IF (TVA.attribute ='Category',TVA.attribute_value,'')) AS Category
 
 FROM `tabSerial No` AS TS inner join
 
-tabItem AS TI ON
+        `tabItem` AS TI ON
 
-TS.item_code = TI.item_code
-
-INNER JOIN `tabItem Price` TIP ON
-
-TS.item_code = TIP.item_code
-
-AND selling = 1 and price_list = 'Standard Selling'
+       TS.item_code = TI.item_code
 
 INNER JOIN  `tabItem Variant Attribute` AS TVA
 
-ON TVA.parent = TI.item_code
+              ON TVA.parent = TI.item_code
+
+LEFT JOIN `tabItem Price` TIP
+
+              ON TS.item_code = TIP.item_code
+
+              AND TIP.selling = 1 and TIP.price_list = 'Standard Selling'
 
 WHERE
 
-reservation_status  = 'Available'
+       TS.reservation_status  = 'Available'
 
-and warehouse is not null
+       and TS.warehouse is not null
 
-and 1= case when %(brand)s ='اختر النوع' then 1 when ( TI.variant_of= %(brand)s) then 1 else 0 end
+       and 1= case when %(brand)s ='اختر النوع' then 1 when ( TI.variant_of= %(brand)s) then 1 else 0 end
 
-and 1= case when %(item_group)s ='اختر المجموعة' then 1 when ( TI.item_group= %(item_group)s) then 1 else 0 end
+       and 1= case when %(item_group)s ='اختر المجموعة' then 1 when ( TI.item_group= %(item_group)s) then 1 else 0 end
 
 group by
 
-warehouse,
+       TS.item_name,
 
-serial_no,
+       TS.warehouse,
 
-TS.item_name,
+       TS.serial_no,
 
-TI.variant_of,
+       TI.variant_of,
 
-TI.item_group
+       TI.item_group
+
 ) T
+
  WHERE
 
-1 = case when %(Color)s ='اختر اللون' THEN 1 when ( T.Color = %(Color)s ) then 1 ELSE 0 END
+       1 = case when %(Color)s ='اختر اللون' THEN 1 when ( T.Color = %(Color)s ) then 1 ELSE 0 END
 
-AND 1= case when %(model)s ='اختر الموديل' THEN 1 when ( T.Model = %(model)s )then 1 else 0 end
+       AND 1= case when %(model)s ='اختر الموديل' THEN 1 when ( T.Model = %(model)s )then 1 else 0 end
 
-AND 1= case when %(Category)s ='اختر الفئة' then 1 when ( T.Category= %(Category)s) then 1 else 0 end""", filters, as_list=1)
+       AND 1= case when %(Category)s ='اختر الفئة' then 1 when ( T.Category= %(Category)s) then 1 else 0 end
+
+group by
+
+       T.CarName,
+
+       T.Warehouse,
+
+       T.SellingRate""", filters, as_list=1)
 
 
 	print car_available
